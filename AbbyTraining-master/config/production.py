221ad5646cfeca3a417d9209e10cc6ff06584d33
Wu Chunlong <wcl6005@126.com>
2017-11-12 22:23:52 +0800

    del

--- AbbyTraining-master/config/production.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 2016-01-13
-
-@author: Wu Wenxiang (wuwenxiang.sh@gmail.com)
-'''
-
-DEBUG = False
-# Override SQLALCHEMY_DATABASE_URI in instance/config.py
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/abbytraining'
--- AbbyTraining-master/frontend/src/app/examples/examples.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('app.examples', [
-            'app.examples.layouts'
-        ]);
-})();
