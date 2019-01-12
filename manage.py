e906ff37cfa41c04e355cc7a54c94ac77581f2c4
Wu Chunlong <wcl6005@126.com>
2019-01-12 20:43:59 +0800

    AccountOnline

--- manage.py
+++ manage.py
@@ -1,79 +1,22 @@
-'''
-Created on 2016-01-17
-
-@author: Wu Wenxiang (wuwenxiang.sh@gmail.com)
-'''
-
-import hashlib
-import logging
+#!/usr/bin/env python
 import os
-import shutil
-import subprocess
 import sys
 
-
-BASE_DIR = os.path.dirname(__file__)
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger("manage.py")
-global app
-
-def showUsage():
-    print("""Usage:
-    python manage.py <Option>
-    python manage.py syncdb    # Create DB
-    python manage.py init      # Init Demo datas
-    python manage.py clean     # Clean virtenv
-    python manage.py prepare   # Prepare virtenv
-    """)
-    sys.exit()
-
-def opt_syncdb():
-#     src = os.path.join(BASE_DIR, 'mysite', 'demo.sqlite3')
-#     dst = os.path.join(BASE_DIR, 'mysite', 'db.sqlite3')
-#     shutil.rmtree(dst, ignore_errors=True)
-#     shutil.copy(src, dst)
-    pass
-
-def opt_init():
-    pass
-
-def opt_clean():
-    ENV_DIR = "env"
-    if os.path.isdir(ENV_DIR):
-        shutil.rmtree(ENV_DIR)
-
-def opt_test():
-    pass
-
-def opt_prepare():
-    _assert_cmd_exist("pip")
-    os.system("pip install virtualenv")
-    
-    opt_syncdb()
-    opt_init()
-    
-    opt_prepare_theme()
-
-def opt_prepare_theme():
-    pass
-
-def _assert_cmd_exist(cmd):
-    try:
-        subprocess.call(cmd)
-    except Exception as e:
-        log.warning("{}->{}".format(type(e), e.message))
-        log.error("Command '{}' not exist!".format(cmd))
-        sys.exit()
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        showUsage()
-
-    selfModule = __import__(__name__)
-    optFunName = "opt_" + sys.argv[1].strip()
-    if optFunName not in selfModule.__dict__:
-        showUsage()
-
-    if BASE_DIR.strip():
-        os.chdir(BASE_DIR)
-    selfModule.__dict__[optFunName](*sys.argv[2:])
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
