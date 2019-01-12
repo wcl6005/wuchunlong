67062511bde243931dc3e45a683650fc1d553bb6
Wu Chunlong <wcl6005@126.com>
2019-01-12 15:42:38 +0800

    AccountOnline

--- mysite/backupdb.py
+++ mysite/backupdb.py
@@ -2,23 +2,21 @@ import logging
 import os
 
 BASE_DIR = os.path.dirname(__file__)
-logging.basicConfig(level=logging.INFO,
-                    filename=os.path.join(BASE_DIR, 'backupdb.log'),
-                    format='%(asctime)s [%(levelname)s] %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
-log = logging.getLogger("backupdb")
 
-cmdStr = (r'cd /home/www/AccountOnline/ZZLARGE-DB-Account-Online '
+cmdStr = (r'cd /home/www/AccountOnline/Mytest '
           '&& rm -rf db db2'
-          '&& dd if=db.txt |openssl des3 -d -k "6yhn(IJN&U*"|tar zxf - '
+          '&& dd if=db.txt |openssl des3 -d -k "8yhn(IJN&U*"|tar zxf - '
           '&& mv db db2 '
-          '&& [ "x$(diff db2/production.sqlite3 ../mysite/production.sqlite3)" != "x" ] '
-          '&& rm -rf /home/www/AccountOnline/ZZLARGE-DB-Account-Online/db '
-          '&& mkdir -p /home/www/AccountOnline/ZZLARGE-DB-Account-Online/db '
-          '&& cp /home/www/AccountOnline/mysite/production.sqlite3 /home/www/AccountOnline/ZZLARGE-DB-Account-Online/db/ '
-          '&& cd /home/www/AccountOnline/ZZLARGE-DB-Account-Online '
+          '&& [ "x$(diff db2/production.sqlite3 ../src/mysite/production.sqlite3)" != "x" ] '
+          '&& rm -rf /home/www/AccountOnline/Mytest/db '
+          '&& mkdir -p /home/www/AccountOnline/Mytest/db '
+          '&& cp /home/www/AccountOnline/src/mysite/production.sqlite3 /home/www/AccountOnline/Mytest/db/ '
+          '&& cd /home/www/AccountOnline/Mytest '
           '&& rm -rf db.txt '
-          '&& tar -zcvf - db|openssl des3 -salt -k "6yhn(IJN&U*" | dd of=db.txt '
+          '&& tar -zcvf - db|openssl des3 -salt -k "8yhn(IJN&U*" | dd of=db.txt '
+          '&& cp db.txt db.txt_`date +%Y-%m-%d[%H:%M:%S]`'
+          '&& rm -rf db db2'
+          '&& git add .'
           '&& git commit -a -m "routinely update"'
           '&& git push')
 
